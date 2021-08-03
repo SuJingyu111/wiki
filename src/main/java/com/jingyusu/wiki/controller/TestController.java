dd83a942b846c5dfacd2ca3447fe35120c969289
SuJingyu111 <gar123su@gmail.com>
2021-08-03 22:22:58 +0800

    Added http client test file test.http

--- src/main/java/com/jingyusu/wiki/controller/TestController.java
+++ src/main/java/com/jingyusu/wiki/controller/TestController.java
@@ -1,13 +1,19 @@
 package com.jingyusu.wiki.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @RequestMapping("/hello")
+    @GetMapping ("/hello")
     public String hello() {
         return "hello world!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post，" + name;
     }
 }
