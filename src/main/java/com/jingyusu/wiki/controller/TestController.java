09074cd1edd7c7a89f6a3d2f066cfdde1c0dafba
SuJingyu111 <gar123su@gmail.com>
2021-08-06 19:09:16 +0800

    Mybatis usage test, developed /test/list interface, query data from Table test

--- src/main/java/com/jingyusu/wiki/controller/TestController.java
+++ src/main/java/com/jingyusu/wiki/controller/TestController.java
@@ -1,15 +1,23 @@
 package com.jingyusu.wiki.controller;
 
+import com.jingyusu.wiki.domain.Test;
+import com.jingyusu.wiki.service.TestService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 public class TestController {
 
     @Value("${test.hello:WARNING:MISSING CONFIG test.hello}")
     private String testHello;
+
+    @Resource
+    private TestService testService;
 
     @GetMapping ("/hello")
     public String hello() {
@@ -19,5 +27,10 @@ public class TestController {
     @PostMapping("/hello/post")
     public String helloPost(String name) {
         return "Hello World! Post，" + name;
+    }
+
+    @GetMapping ("/test/list")
+    public List<Test> list() {
+        return testService.list();
     }
 }
---
+++ src/main/java/com/jingyusu/wiki/mapper/TestMapper.java
@@ -0,0 +1,10 @@
+package com.jingyusu.wiki.mapper;
+
+import com.jingyusu.wiki.domain.Test;
+
+import java.util.List;
+
+public interface TestMapper {
+
+    public List<Test> list();
+}
