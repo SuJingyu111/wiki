d2893942b3fab38768745ccc9794030b9a7257bd
SuJingyu111 <gar123su@gmail.com>
2021-08-08 23:24:37 +0800

    Encapsulated request & return parameters.

--- src/main/java/com/jingyusu/wiki/service/EbookService.java
+++ src/main/java/com/jingyusu/wiki/service/EbookService.java
@@ -1,10 +1,15 @@
 package com.jingyusu.wiki.service;
 
 import com.jingyusu.wiki.domain.Ebook;
+import com.jingyusu.wiki.domain.EbookExample;
 import com.jingyusu.wiki.mapper.EbookMapper;
+import com.jingyusu.wiki.req.EbookReq;
+import com.jingyusu.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,7 +18,17 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list() {
-        return ebookMapper.selectByExample(null);
+    public List<EbookResp> list(EbookReq req) {
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%" + req.getName() + "%");
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        List<EbookResp> respList = new ArrayList<>();
+        for (Ebook ebook : ebookList) {
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook, ebookResp);
+            respList.add(ebookResp);
+        }
+        return respList;
     }
 }
